56fc3dda1e119a2ab5c23356dbf7c60e334869b2
Th0u305 <ahnaf4411@gmail.com>
2025-08-03 03:52:28 +0600

    ddd

--- src/app/modules/admin/admin.service.ts
+++ src/app/modules/admin/admin.service.ts
@@ -107,9 +107,9 @@ const walletAction = async (action : string , userId: string) => {
         throw new AppError(httpStatus.NOT_FOUND, "This account doesn't exists")
     }
 
-    if (action.toUpperCase() !== "ACTIVE" || action.toUpperCase() !== "SUSPENDED" || action.toUpperCase() !== "BLOCKED") {
-        throw new AppError(httpStatus.BAD_REQUEST, "Please use a correct wallet action (e.g., ACTIVE, BLOCKED, SUSPENDED)");
-    }
+    // if (action.toUpperCase() !== "ACTIVE" || action.toUpperCase() !== "SUSPENDED" || action.toUpperCase() !== "BLOCKED") {
+    //     throw new AppError(httpStatus.BAD_REQUEST, "Please use a correct wallet action (e.g., ACTIVE, BLOCKED, SUSPENDED)");
+    // }
 
     const session = await mongoose.startSession();
 
